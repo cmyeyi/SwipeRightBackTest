9a965c7d447fde984ce88c1dd3b5c1211a587548
heqiang <heqiang1@cmcm.com>
2020-03-06 11:35:32 +0800

    方法提取

--- app/src/main/java/com/dlong/rep/swiperightbacktest/MainActivity.java
+++ app/src/main/java/com/dlong/rep/swiperightbacktest/MainActivity.java
@@ -1,26 +1,34 @@
 package com.dlong.rep.swiperightbacktest;
 
-import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
 import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 
 import java.util.List;
 
-import static com.dlong.rep.swiperightbacktest.MessengerService.MSG_FROM_CLIENT;
-
 public class MainActivity extends BaseActivity {
     private Context mContext = this;
     private Intent mIntent;
 
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_getBookList:
+                    getBookList();
+                    break;
+                case R.id.btn_addBook_inOut:
+                    addBook();
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,12 +64,12 @@ public class MainActivity extends BaseActivity {
 
 
     List<Book> bookList;
-    private IBookMananger bookMananger;
+    private IBookMananger bookManager;
     private boolean connected;
     private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            bookMananger = IBookMananger.Stub.asInterface(service);
+            bookManager = IBookMananger.Stub.asInterface(service);
             connected = true;
         }
 
@@ -84,35 +92,31 @@ public class MainActivity extends BaseActivity {
         }
     }
 
-    private View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.btn_getBookList:
-                    if (connected) {
-                        try {
-                            bookList = bookMananger.getBookList();
-                            for (Book b : bookList) {
-                                Log.e("#####", "书名：" + b.getName());
-                            }
-                        } catch (RemoteException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    break;
-                case R.id.btn_addBook_inOut:
-                    if (connected) {
-                        Book book = new Book(10011, "这是一本新书 InOut");
-                        try {
-                            bookMananger.addBook(book);
-                            Log.e("#####", "向服务器以InOut方式添加了一本新书");
-                            Log.e("#####", "新书名：" + book.getName());
-                        } catch (RemoteException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    break;
+
+    private void getBookList() {
+        if (connected) {
+            try {
+                bookList = bookManager.getBookList();
+                for (Book b : bookList) {
+                    Log.e("#####", "书名：" + b.getName());
+                }
+            } catch (RemoteException e) {
+                e.printStackTrace();
             }
         }
-    };
+    }
+
+    private void addBook() {
+        if (connected) {
+            Book book = new Book(10011, "这是一本新书 InOut");
+            try {
+                bookManager.addBook(book);
+                Log.e("#####", "向服务器以InOut方式添加了一本新书");
+                Log.e("#####", "新书名：" + book.getName());
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
